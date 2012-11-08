caa34382ae33cfd1c16b4d8b39360167b29655db
Lance Speelmon <lance@speelmon.com>
2012-11-08 14:34:53 -0700

    Listing 5.36. A test for the links on the layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,51 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title(page_title)) }
   end
 
+  describe "Navigation" do
+    before { visit root_path }
+
+    it "should have the right links on the layout" do
+      click_link "About" do
+        let(:heading)    { 'About' }
+        let(:page_title) { 'About Us' }
+
+        it_should_behave_like "all static pages"
+      end
+      click_link "Help" do
+        let(:heading)    { 'Help' }
+        let(:page_title) { heading }
+
+        it_should_behave_like "all static pages"
+      end
+      click_link "Contact" do
+        let(:heading)    { 'Contact' }
+        let(:page_title) { heading }
+
+        it_should_behave_like "all static pages"
+      end
+      click_link "Home" do
+        let(:heading)    { 'Sample App' }
+        let(:page_title) { '' }
+
+        it_should_behave_like "all static pages"
+        it { should_not have_selector 'title', text: '| Home' }
+      end
+      click_link "sample app" do
+        let(:heading)    { 'Sample App' }
+        let(:page_title) { '' }
+
+        it_should_behave_like "all static pages"
+        it { should_not have_selector 'title', text: '| Home' }
+      end
+      click_link "Sign up now!" do
+        let(:heading)    { 'Sign up' }
+        let(:page_title) { heading }
+
+        it_should_behave_like "all static pages"
+      end
+    end
+  end
+
   describe "Home page" do
     before { visit root_path }
 
