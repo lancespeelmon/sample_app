3664ba7bb9f1702384951bd5ee19efd6975743e3
Lance Speelmon <lance@speelmon.com>
2012-11-12 17:02:23 -0700

    8.2 Signin success: rails generate migration add_remember_token_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109032524) do
+ActiveRecord::Schema.define(:version => 20121113000039) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
