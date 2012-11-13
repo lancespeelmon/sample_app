26cf59f1ff5073279718df0396ce31c91d24de44
Lance Speelmon <lance@speelmon.com>
2012-11-13 12:26:58 -0700

    9.1.2 Unsuccessful edits

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,17 +29,12 @@ describe "Authentication" do
   end
 
   describe "with valid information" do
-    # before { visit signin_path }
     let(:user) { FactoryGirl.create(:user) }
-    before do
-      visit signin_path
-      fill_in "Email",    with: user.email
-      fill_in "Password", with: user.password
-      click_button "Sign in"
-    end
+    before { sign_in user }
 
     it { should have_selector('title', text: user.name) }
     it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Settings', href: edit_user_path(user)) }
     it { should have_link('Sign out', href: signout_path) }
     it { should_not have_link('Sign in', href: signin_path) }
 
